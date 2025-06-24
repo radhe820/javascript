5b906f29a68ac40674ca207b7f209b930d94154a
radhe820 <radheshyam21204@gmail.com>
2025-06-24 11:45:52 +0000

    dataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";  //treat all js code as newer version
+
+// alert ( 3 + 3 ) // we are using nodejs, not browser
+
+
+// java script data types
+ 
+let name ="hitesh"
+let age = 18
+let isLoggedIn = false
+
+
+/* premetive data types
+
+number = 2 to the power of 53
+bigint
+string = " "
+boolean = true/false
+null = standalone value
+undefined
+symbol = unique
+
+*/
+
+console.log(typeof "hitesh" );
+console.log(typeof false );
+console.log(typeof 18 );
+
+
+console.log(typeof undefined );   //undefined
+console.log(typeof null );  //object
