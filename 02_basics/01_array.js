add2061cbfa2e5b6daaee225418dd69b90d57488
radhe820 <radheshyam21204@gmail.com>
2025-06-30 15:50:15 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,43 @@
+//  array
+
+const myArr = [1 , 2 , 3 , 4 , 5 , 6]
+const myHeros = ["shaktimaan" , "naagraj"]
+
+const myArr2 = new Array(1 , 2 , 3 , 4)
+console.log(myArr[1]);
+
+// Array methods
+
+myArr.push(7)
+myArr.push(8)
+myArr.pop()
+console.log(myArr);
+
+myArr.unshift(9)
+myArr.shift()
+console.log(myArr);
+
+
+console.log((myArr.includes(7)));
+console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+console.log(myArr);
+
+console.log(newArr);
+console.log(typeof newArr);
+
+
+/*'''''''''''''' slice &&&&&& splice ''''''''''''''''''*/
+
+console.log(("A ", myArr));
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ",myArr);
+
+
+const myn2 = myArr.splice(1,3)
+console.log("C ",myArr);
+console.log(myn2);
