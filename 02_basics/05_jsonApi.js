89179b5de877fa7fb3a161eaf16dfd4fae4e18fd
radhe820 <radheshyam21204@gmail.com>
2025-07-04 14:11:39 +0530

    jsonApi introduction

---
+++ 02_basics/05_jsonApi.js
@@ -0,0 +1,31 @@
+// introduction to json Api
+
+
+// Api in object form
+
+
+{
+    "name" = "radhe"
+    "courseName" = "js in hindi"
+    "price" = "free"
+}
+
+
+
+// Api in array form
+
+[
+    
+    {
+
+    }
+
+    {
+
+    }
+
+    {
+
+    }
+
+]
